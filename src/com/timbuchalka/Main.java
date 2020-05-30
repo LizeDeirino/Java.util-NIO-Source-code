82f900329183cbc82cc38024b54c0b5ab7f3f103
LizeDeirino <65598384+LizeDeirino@users.noreply.github.com>
2020-05-30 14:56:21 +0200

    Add files via upload

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -1,81 +1,105 @@
 package com.timbuchalka;
 
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-	    try(FileOutputStream binFile = new FileOutputStream("data.dat");
-            FileChannel binChannel = binFile.getChannel()) {
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    public boolean accept(Path path) throws IOException {
+//                        return (Files.isRegularFile(path));
+//                    }
+//                };
 
-            byte[] outputBytes = "Hello World!".getBytes();
-            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
-            int numBytes = binChannel.write(buffer);
-            System.out.println("numBytes written was: " + numBytes);
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
 
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            numBytes = binChannel.write(intBuffer);
-            System.out.println("numBytes written was: " + numBytes);
-
-            intBuffer.flip();
-            intBuffer.putInt(-98765);
-            intBuffer.flip();
-            numBytes = binChannel.write(intBuffer);
-            System.out.println("numBytes written was: " + numBytes);
-
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            FileChannel channel = ra.getChannel();
-            outputBytes[0] = 'a';
-            outputBytes[1] = 'b';
-            buffer.flip();
-            long numBytesRead = channel.read(buffer);
-            if(buffer.hasArray()) {
-                System.out.println("byte buffer = " + new String(buffer.array()));
+        Path directory = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir2");
+//        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");  // FileTree\\Dir2 (windows)
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
             }
 
-            // Absolute read
-            intBuffer.flip();
-            numBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
-            intBuffer.flip();
-            numBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
+        } catch (IOException | DirectoryIteratorException e) {
+            System.out.println(e.getMessage());
+        }
 
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
 
-            // Relative read
-            //            intBuffer.flip();
-            //            numBytesRead = channel.read(intBuffer);
-            //            intBuffer.flip();
-            //            System.out.println(intBuffer.getInt());
-            //            intBuffer.flip();
-            //            numBytesRead = channel.read(intBuffer);
-            //            intBuffer.flip();
-            //            System.out.println(intBuffer.getInt());
-
-            channel.close();
-            ra.close();
-
-//            System.out.println("outputBytes = " + new String(outputBytes));
-
-//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-//            byte[] b = new byte[outputBytes.length];
-//            ra.read(b);
-//            System.out.println(new String(b));
-//
-//            long int1 = ra.readInt();
-//            long int2 = ra.readInt();
-//            System.out.println(int1);
-//            System.out.println(int2);
+        try {
+            Path tempFile = Files.createTempFile("myapp", ".appext");  // C:\Users\???\AppData\Local\Temp\myapp1797805585146820741.appext
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
 
         } catch(IOException e) {
-	        e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for(FileStore store : stores) {
+            System.out.println("Volume name/Drive letter = " + store);
+            System.out.println("file store = " + store.name());
+        }
+
+        System.out.println("*******************");
+        Iterable<Path> rootPaths = FileSystems.getDefault().getRootDirectories();
+        for(Path path : rootPaths) {
+            System.out.println(path);
+        }
+
+        System.out.println("---Walking Tree for Dir2---");
+        Path dir2Path = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir2");
+        try {
+            Files.walkFileTree(dir2Path, new PrintNames());
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("---Copy Dir2 to Dir4/Dir2Copy---");
+        Path copyPath = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir4" + File.separator + "Dir2Copy");
+        //    FileTree/Dir4/Dir2Copy
+        try {
+            Files.walkFileTree(dir2Path, new CopyFiles(dir2Path, copyPath));
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        File file = new File("/Examples/file.txt");   // C:\\Examples\file.txt
+        Path convertedPath = file.toPath();
+        System.out.println("convertedPath = " + convertedPath);
+
+        File parent = new File("/Examples");  // C:\\Examples
+        File resolvedFile = new File(parent, "dir/file.txt");  // dir\\file.txt
+        System.out.println(resolvedFile.toPath());
+
+        resolvedFile = new File("/Examples", "dir/file.txt");  // C:\\Examples   dir\\file.txt
+        System.out.println(resolvedFile.toPath());
+
+        Path parentPath = Paths.get("/Examples");  // C:\\Examples
+        Path childRelativePath = Paths.get("dir/file.txt");  // dir\\file.txt
+        System.out.println(parentPath.resolve(childRelativePath));
+
+        File workingDirectory = new File("").getAbsoluteFile();
+        System.out.println("Working directory = " + workingDirectory.getAbsolutePath());
+
+        System.out.println("--- print Dir2 contents using list() ---");
+        File dir2File = new File(workingDirectory, "/FileTree/Dir2");   // \\FileTree\Dir2
+        String[] dir2Contents = dir2File.list();
+        for(int i=0; i< dir2Contents.length; i++) {
+            System.out.println("i= " + i + ": " + dir2Contents[i]);
+        }
+
+        System.out.println("--- print Dir2 contents using listFiles() ---");
+        File[] dir2Files = dir2File.listFiles();
+        for(int i=0; i< dir2Files.length; i++) {
+            System.out.println("i= " + i + ": " + dir2Files[i].getName());
         }
     }
 }
